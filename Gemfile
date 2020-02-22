32597db9212b69f9ee568fb9e3c354defaf97194
Gareth Rushgrove <gareth@morethanseven.net>
2020-02-22 16:40:51 +0000

    Add basic tests to the Sinatra application

--- Gemfile
+++ Gemfile
@@ -5,3 +5,9 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem "sinatra", "2.0.1"
+
+group :test do
+  gem 'rake'
+  gem 'rspec'
+  gem 'rack-test'
+end
---
+++ spec/app_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../app.rb'
+require 'rspec'
+require 'rack/test'
+
+set :environment, :test
+
+describe 'Server Service' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "should load the page" do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include("Hello world!")
+  end
+end
